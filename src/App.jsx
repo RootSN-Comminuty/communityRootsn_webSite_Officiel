689044f68e1683a82e42be1b7553177a36426b84
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2023-06-26 17:45:24 +0000

    All Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Gallery from './pages/gallery/Gallery'
+import Ministers from './pages/ministers/Ministers'
+import Blocs from './pages/blocs/Blocs'
+import Contact from './pages/contact/Contact'
+import NotFound from './pages/notFound/NotFound'
+
 
 const App = () => {
     return (
-        <div>RootSN-Community web site</div>
+        <div>
+            <Home />
+            <About />
+            <Gallery />
+            <Ministers />
+            <Blocs />
+            <Contact />
+            <NotFound />
+        </div>
     )
 }
 
