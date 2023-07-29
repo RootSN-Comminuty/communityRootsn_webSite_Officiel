31c1d5aa934ffd34a65dc29c5c5c7db012621522
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2023-07-29 01:01:10 +0000

    Add Fourth Section And Update code Third Section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import MainHeader from '../../components/MainHeader'
 import Programs from '../../components/Programs'
 import Values from '../../components/Values'
+import FAQs from '../../components/FAQs'
 
 
 import './home.css'
@@ -11,6 +12,7 @@ const Home = () => {
             <MainHeader />
             <Programs />
             <Values />
+            <FAQs />
         </>
     )
 }
